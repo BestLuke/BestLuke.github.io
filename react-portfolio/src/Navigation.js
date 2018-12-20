eb7f1bf9c2f679977a5e5f9facc9c31943f88d9c
mrmimosizer <m1mosizer@gmail.com>
2018-12-20 18:46:06 +1100

    added sticky navbar

--- react-portfolio/src/Navigation.js
+++ react-portfolio/src/Navigation.js
@@ -2,7 +2,39 @@ import React, { Component } from 'react';
 import './Navigation.css';
 
 class Navigation extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state={};
+
+        this.handleScroll = this.handleScroll.bind(this);
+    }
+
+    handleScroll() {
+        this.setState({scroll: window.scrollY});
+    }
+
+    componentDidMount() {
+        const el = document.querySelector('nav');
+        this.setState({top: el.offsetTop, height: el.offsetHeight});
+        window.addEventListener('scroll', this.handleScroll);
+    }
+
+    componentDidUpdate() {
+        this.state.scroll > this.state.top ? document.body.style.paddingTop = `${this.state.height}px` : document.body.style.paddingTop = 0;
+    }
+
   render() {
+      let classes;
+      function handleScroll() {
+          if(window.scrollY > 0) {
+              console.log("scrolled");
+              classes = 'fixed-nav';
+              console.log(classes)
+          }
+      }
+      window.addEventListener('scroll', handleScroll);
+
       const section=['Home', 'About', 'services', 'Portfolio', 'Contact'];
       const navLinks = section.map( section => {
         return (
@@ -10,7 +42,7 @@ class Navigation extends Component {
         )
       });
     return (
-        <nav>
+        <nav className={this.state.scroll > this.state.top ? "fixed-nav" : ""}>
             <h2 className='logo'>{this.props.LogoTitle}</h2>
             <ul>
                 {navLinks}
--- react-portfolio/src/Services.js
+++ react-portfolio/src/Services.js
@@ -8,38 +8,49 @@ render() {
             <h3>Services</h3>
             <h2>What We Offer</h2>
             <div className="row">
-                <div>
-                    <span>
-                        <i class="fas fa-mobile-alt"></i>
-                    </span>
-                    <h4>Responsive</h4>
-                    <p>Look great on my screen size!</p>
-                </div>
-                <div>
-                    <span>
-                        <i class="fas fa-pencil-alt"></i>
-                    </span>
-                    <h4>Redesigned</h4>
-                    <p>Freshly redisigned for bootstrap 4</p>
-                </div>
-                <div>
-                    <span>
-                        <i class="fas fa-thumbs-up"></i>                        
-                    </span>
-                    <h4>Favourited</h4>
-                    <p>Millions of users <i class="fas fa-heart"></i> Start using Bootstrap</p>
-                </div>
-                <div>
-                    <span>
-                        <i class="fas fa-brain"></i>
-                    </span>
-                    <h4>Question</h4>
-                    <p>Pick my Brain</p>
-                </div>
+                <Icons myIcon={iconObj[0].icon} myTitle={iconObj[0].title} myDescription={iconObj[0].description} />
+                <Icons myIcon={iconObj[1].icon} myTitle={iconObj[1].title} myDescription={iconObj[1].description} />
+                <Icons myIcon={iconObj[2].icon} myTitle={iconObj[2].title} myDescription={iconObj[2].description} />
+                <Icons myIcon={iconObj[3].icon} myTitle={iconObj[3].title} myDescription={iconObj[3].description} />
             </div>
         </div>
     );
 }
+}
+
+const iconObj = [
+    {
+        icon: <i class="fas fa-mobile-alt"></i>,
+        title: 'Responsive',
+        description: 'Look great on my screen size!'
+    },
+    {
+        icon: <i class="fas fa-pencil-alt"></i>,
+        title: 'Redesigned',
+        description: 'Freshly redisigned for bootstrap 4'
+    },
+    {
+        icon: <i class="fas fa-thumbs-up"></i>,
+        title: 'Favourited',
+        description: 'Millions of users <i class="fas fa-heart"></i> Start using Bootstrap'
+    },
+    {
+        icon: <i class="fas fa-brain"></i>,
+        title: 'Question',
+        description: 'Pick my brain'
+    }
+]
+
+class Icons extends Component {
+    render() {
+        return (
+            <div>
+            <span>{this.props.myIcon}</span>
+            <h4>{this.props.myTitle}</h4>
+            <p>{this.props.myDescription}</p>
+        </div>
+        )
+    }
 }
 
 export default Services;
